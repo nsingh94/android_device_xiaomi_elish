f93f0f305b077710f64b48c8b14b9cd49e3ef5d7
nsingh94 <namit1994@gmail.com>
2023-08-27 19:20:48 -0400

    elish: disable prebuilt kernel

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -26,7 +26,7 @@ TARGET_KERNEL_CONFIG += vendor/xiaomi/elish.config
 BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 
 # Kernel - prebuilt
-TARGET_FORCE_PREBUILT_KERNEL := true
+# TARGET_FORCE_PREBUILT_KERNEL := true
 ifeq ($(TARGET_FORCE_PREBUILT_KERNEL),true)
 TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilts/kernel
 TARGET_PREBUILT_DTB := $(DEVICE_PATH)/prebuilts/dtb.img
