d16d9895d7da2546c0c4b8f824d7fea1473820f3
nsingh94 <namit1994@gmail.com>
2023-10-05 21:53:10 -0400

    elish: use high density artwork

--- lineage_elish.mk
+++ lineage_elish.mk
@@ -22,8 +22,10 @@ PRODUCT_MODEL := M2105K81AC
 PRODUCT_CHARACTERISTICS := tablet
 
 # AAPT
+# Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
-PRODUCT_AAPT_PREF_CONFIG := xhdpi
+PRODUCT_AAPT_PREF_CONFIG := xxxhdpi
+PRODUCT_AAPT_PREBUILT_DPI := xxxhdpi xxhdpi xhdpi hdpi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
