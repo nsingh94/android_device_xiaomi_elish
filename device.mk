9bd1305f09f7483e0f45b8ee90692ee391b28e31
nsingh94 <namit1994@gmail.com>
2023-10-07 10:14:25 -0400

    elish: build gps HAL

--- device.mk
+++ device.mk
@@ -213,6 +213,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0.vendor
 
+# GNSS
+PRODUCT_PACKAGES += \
+    android.hardware.gnss@1.1.vendor \
+    android.hardware.gnss@2.1.vendor
+
 # Health
 PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl \
