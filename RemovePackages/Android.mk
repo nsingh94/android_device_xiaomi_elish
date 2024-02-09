6db773e963140e1d2d7902b28d9e231a1b2901c9
nsingh94 <namit1994@gmail.com>
2024-02-09 14:17:56 -0500

    elish: don't include PixelExperience messages app. Remove package overrides to fix Android System Intelligence crash on PixelExperience

--- RemovePackages/Android.mk
+++ RemovePackages/Android.mk
@@ -5,18 +5,12 @@ LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
 LOCAL_OVERRIDES_PACKAGES := \
-   BlockedNumberProvider \
-   CarrierServices \
    com.google.android.dialer.support \
    Dialer \
    GoogleDialer \
    messaging \
-   MmsService \
-   SimAppDialog \
-   Stk \
-   TeleService \
-   Telecom \
-   TelephonyProvider
+   PrebuiltBugle \
+   Stk
 
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
