b09d801e099283a4a3399eb211ddce7e773e4d34
nsingh94 <namit1994@gmail.com>
2026-03-04 08:33:27 -0500

    elish: build fingerprint blob vendor.xiaomi.hardware.fx.tunnel from source

--- extract-files.py
+++ extract-files.py
@@ -66,6 +66,7 @@ blob_fixups: blob_fixups_user_type = {
 namespace_imports = [
     'hardware/qcom-caf/common/libqti-perfd-client',
     'hardware/qcom-caf/sm8250',
+    'hardware/xiaomi',
     'vendor/qcom/opensource/display',
     'vendor/xiaomi/sm8250-common',
 ]
