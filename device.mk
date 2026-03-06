bcdc1afedb1528013b28c1052dc241d577af1eeb
nsingh94 <namit1994@gmail.com>
2026-03-06 12:06:32 -0500

    elish: build Xiaomi fingerprint AIDL

--- device.mk
+++ device.mk
@@ -33,6 +33,13 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     libpiex_shim
 
+# Fingerprint
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint-service.xiaomi
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
+
 # Overlays
 PRODUCT_PACKAGES += \
     ApertureOverlayDevice \
