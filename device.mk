bd179c8e7dde05cc13fea8a6f4d3d173a08b2b27
nsingh94 <namit1994@gmail.com>
2026-03-11 16:59:50 -0400

    elish: add SettingsProvider overlay with friendly device name

--- device.mk
+++ device.mk
@@ -47,6 +47,7 @@ PRODUCT_PACKAGES += \
     LineageDialerOverlayDevice \
     LineageSettingsOverlayDevice \
     LineageSystemUIOverlayDevice \
+    SettingsProviderOverlayDevice \
     SystemUIOverlayDevice
 
 # Permissions
