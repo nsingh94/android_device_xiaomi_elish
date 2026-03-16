7ac90e27edb29e41ec8c0a355197c2c43def5c9d
nsingh94 <namit1994@gmail.com>
2026-03-16 13:23:05 -0400

    elish: sepolicy: Label sysfs nodes for elish

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -27,5 +27,8 @@ TARGET_OTA_ASSERT_DEVICE := elish
 # Properties
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
+# Sepolicy
+BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+
 # Inherit from the proprietary version
 include vendor/xiaomi/elish/BoardConfigVendor.mk
